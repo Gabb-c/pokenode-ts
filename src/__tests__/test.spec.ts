cea7a41d5346a04a3069895f9172ace029d7eeaf
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-05-07 00:44:35 -0300

    fix: types

---
+++ src/__tests__/test.spec.ts
@@ -0,0 +1,8 @@
+describe('Test Home component', () => {
+  it('Check if component Home is rendered', () => {
+    const myName = 'Gabriel';
+
+    expect(myName).toBe('Gabriel');
+  });
+});
+export {};
