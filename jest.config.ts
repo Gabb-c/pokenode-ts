6238536c1ce4385e497445771edae2642da87af3
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-05-07 01:29:14 -0300

    fix: check types in build workflow

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ export default {
     '**/*.{ts}',
     '!**/*.d.ts', //  Exclude all type declaration files
     '!**/node_modules/**', //  Exclude all files in node_modules
-    '!**/dist/**', //  Exclude all files in the .next folder
+    '!**/dist/**', //  Exclude all files in the dist folder
   ],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
