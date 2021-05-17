921705855074539aa9ce1348f7d64a40bf848e42
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-05-17 12:06:33 -0300

    fix: exclude all interface files from coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,8 @@ export default {
     '**/*.{ts}',
     '!**/*.d.ts', //  Exclude all type declaration files
     '!**/node_modules/**', //  Exclude all files in node_modules
-    '!**/dist/**', //  Exclude all files in the dist folder
+    '!**/dist/**', //  Exclude all files in dist folder
+    '!**/src/models/**', //  Exclude all files in models
   ],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
