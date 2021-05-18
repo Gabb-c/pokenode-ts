2e0e1d246819a558c24653a7adcd623bde9736be
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-05-18 20:33:58 -0300

    feat: add berry firmness methods to berry client

--- src/__tests__/berry.spec.ts
+++ src/__tests__/berry.spec.ts
@@ -1,7 +1,8 @@
-import { Berry, NamedAPIResourceList } from '../models';
+import { Berry, BerryFirmness, NamedAPIResourceList } from '../models';
 import { BerryClient } from '../clients/berry.client';
 
 describe('Test Berry Client', () => {
+  // Berry
   it('Check if it returns a berry passig a name', async () => {
     const client = new BerryClient();
 
@@ -20,6 +21,32 @@ describe('Test Berry Client', () => {
     const client = new BerryClient();
 
     const data = await client.listBerries().then((response: NamedAPIResourceList) => response);
+
+    expect(data.results.length).toBeGreaterThan(0);
+  });
+  // Berry Firmness
+  it('Check if it returns a berry firmness passig a name', async () => {
+    const client = new BerryClient();
+
+    const data = await client
+      .getBerryFirmnessByName('very-soft')
+      .then((response: BerryFirmness) => response);
+
+    expect(data.name).toBe('very-soft');
+  });
+  it('Check if it returns a berry firmness passing an ID', async () => {
+    const client = new BerryClient();
+
+    const data = await client.getBerryFirmnessById(1).then((response: BerryFirmness) => response);
+
+    expect(data.name).toBe('very-soft');
+  });
+  it('Check if it returns a list of berry firmnesses', async () => {
+    const client = new BerryClient();
+
+    const data = await client
+      .listBerryFirmness()
+      .then((response: NamedAPIResourceList) => response);
 
     expect(data.results.length).toBeGreaterThan(0);
   });
--- src/clients/berry.client.ts
+++ src/clients/berry.client.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
 import { AxiosError, AxiosResponse } from 'axios';
-import { Berry, NamedAPIResourceList } from '../models';
+import { Berry, BerryFirmness, NamedAPIResourceList } from '../models';
 import client from '../config/axios';
 import { Endpoints } from '../constants/endpoints';
 
@@ -45,6 +45,42 @@ export class BerryClient {
   }
 
   /**
+   * Get a Berry Firmness by it's ID
+   * @param id The Berry ID
+   * @returns Berry Firmness
+   */
+  public getBerryFirmnessById(id: number): Promise<BerryFirmness> {
+    return new Promise<BerryFirmness>((resolve, reject) => {
+      this.api
+        .get(`${Endpoints.BerryFirmness}/${id}`)
+        .then((response: AxiosResponse<BerryFirmness>) => {
+          resolve(response.data);
+        })
+        .catch((error: AxiosError<string>) => {
+          reject(error);
+        });
+    });
+  }
+
+  /**
+   * Get a Berry Firmness by it's ID
+   * @param name The Berry name
+   * @returns Berry Firmness
+   */
+  public getBerryFirmnessByName(name: string): Promise<BerryFirmness> {
+    return new Promise<BerryFirmness>((resolve, reject) => {
+      this.api
+        .get(`${Endpoints.BerryFirmness}/${name}`)
+        .then((response: AxiosResponse<BerryFirmness>) => {
+          resolve(response.data);
+        })
+        .catch((error: AxiosError<string>) => {
+          reject(error);
+        });
+    });
+  }
+
+  /**
    * List berries
    * @param offset The first item that you will get
    * @param limit How many berries per page
@@ -54,6 +90,25 @@ export class BerryClient {
     return new Promise<NamedAPIResourceList>((resolve, reject) => {
       this.api
         .get(`${Endpoints.Berry}?offset=${offset}&limit=${limit}`)
+        .then((response: AxiosResponse<NamedAPIResourceList>) => {
+          resolve(response.data);
+        })
+        .catch((error: AxiosError<string>) => {
+          reject(error);
+        });
+    });
+  }
+
+  /**
+   * List berries
+   * @param offset The first item that you will get
+   * @param limit How many berry firmnesses per page
+   * @returns A list of berry firmnesses
+   */
+  public listBerryFirmness(offset?: number, limit?: number): Promise<NamedAPIResourceList> {
+    return new Promise<NamedAPIResourceList>((resolve, reject) => {
+      this.api
+        .get(`${Endpoints.BerryFirmness}?offset=${offset}&limit=${limit}`)
         .then((response: AxiosResponse<NamedAPIResourceList>) => {
           resolve(response.data);
         })
