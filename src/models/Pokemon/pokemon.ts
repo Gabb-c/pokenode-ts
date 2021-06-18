cca24af45d57d9a907210fc2febfa8534103329f
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-06-18 12:43:38 -0300

    fix(pokemon-model): pokemon color typings

--- src/models/Pokemon/pokemon.ts
+++ src/models/Pokemon/pokemon.ts
@@ -312,7 +312,17 @@ export interface PokemonColor {
   /** The identifier for this resource */
   id: number;
   /** The name for this resource */
-  name: string;
+  name:
+    | 'black'
+    | 'blue'
+    | 'brown'
+    | 'gray'
+    | 'green'
+    | 'pink'
+    | 'purple'
+    | 'red'
+    | 'white'
+    | 'yellow';
   /** The name of this resource listed in different languages */
   names: Name[];
   /** A list of the Pokémon species that have this color */
