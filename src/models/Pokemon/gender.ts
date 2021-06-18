ceca7f75202b278fd594ef80021ddb1bef686fd2
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-06-18 13:08:59 -0300

    fix(pokemon-model): gender typings

--- src/models/Pokemon/gender.ts
+++ src/models/Pokemon/gender.ts
@@ -10,7 +10,7 @@ export interface Gender {
   /** The identifier for this resource */
   id: number;
   /** The name for this resource */
-  name: string;
+  name: 'male' | 'female' | 'genderless';
   /** A list of Pokémon species that can be this gender and how likely it is that they will be */
   pokemon_species_details: PokemonSpeciesGender[];
   /** A list of Pokémon species that required this gender in order for a Pokémon to evolve into them */
