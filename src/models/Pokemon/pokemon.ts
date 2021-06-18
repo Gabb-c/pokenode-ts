a3eb485800ff72dbb28a597980a76a4cb741b1f2
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-06-18 12:58:51 -0300

    fix(pokemon-model): pokemon habitat typings

--- src/models/Pokemon/pokemon.ts
+++ src/models/Pokemon/pokemon.ts
@@ -399,7 +399,16 @@ export interface PokemonHabitat {
   /** The identifier for this resource */
   id: number;
   /** The name for this resource */
-  name: string;
+  name:
+    | 'cave'
+    | 'forest'
+    | 'grassland'
+    | 'mountain'
+    | 'rare'
+    | 'rough-terrain'
+    | 'sea'
+    | 'urban'
+    | 'waters-edge';
   /** The name of this resource listed in different languages */
   names: Name[];
   /** A list of the Pokémon species that can be found in this habitat */
