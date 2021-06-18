cfa1a61c41e3f9dbc29b3c4bf8652cf394eaa1a5
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-06-18 18:54:08 -0300

    fix(evolution-model): evolution chain typings

--- src/models/Evolution/evolution.ts
+++ src/models/Evolution/evolution.ts
@@ -76,7 +76,7 @@ export interface EvolutionChain {
    * The item that a Pokémon would be holding when mating that would trigger
    * the egg hatching a baby Pokémon rather than a basic Pokémon
    */
-  baby_trigger_item: NamedAPIResource;
+  baby_trigger_item: NamedAPIResource | null;
   /**
    * The base chain link object. Each link contains evolution details for a Pokémon in the chain.
    * Each link references the next Pokémon in the natural evolution order
