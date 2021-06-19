54e72b5958b251c8c0e6fd3d595eb41a94c3bef1
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-06-19 11:03:19 -0300

    fix(jest-config): coverage range

--- jest.config.ts
+++ jest.config.ts
@@ -4,13 +4,13 @@ export default {
   testEnvironment: 'node',
   collectCoverageFrom: [
     '**/*.{ts}',
-    '!**/*.d.ts', //  Exclude all type declaration files
-    '!**/node_modules/**', //  Exclude all files in node_modules
-    '!**/dist/**', //  Exclude all files in dist folder
-    '!**/src/models/**', //  Exclude all files in models
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/src/models/**',
     '!**/src/constants/**',
     '!**/src/config/**',
-    '**/src/structures/**',
+    '!**/src/structures/**',
   ],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
