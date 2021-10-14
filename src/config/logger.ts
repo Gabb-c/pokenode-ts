a46c5c59eaf0bcbca7f4982364474ed810c8a8ae
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-10-14 12:28:52 -0300

    fix(base): update logger and base client to pino 7.0.1

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,30 +1,33 @@
 import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-import pino, { LoggerOptions, Logger } from 'pino';
+import pino from 'pino';
 
-export const createLogger = (options: LoggerOptions): Logger => pino(options);
+export const createLogger = (options: pino.LoggerOptions): pino.Logger => pino(options);
 
-export const handleRequest = (config: AxiosRequestConfig, logger: Logger): AxiosRequestConfig => {
+export const handleRequest = (
+  config: AxiosRequestConfig,
+  logger: pino.Logger
+): AxiosRequestConfig => {
   logger.info(`[ Request Config ] ${config.method?.toUpperCase() || ''} | ${config.url || ''}`);
   return config;
 };
 
 export const handleRequestError = async (
   error: AxiosError<unknown>,
-  logger: Logger
+  logger: pino.Logger
 ): Promise<AxiosError<unknown>> => {
   logger.error(`[ Request Error ] ${error.code || 'UNKNOWN'} | ${error.message}`);
   return Promise.reject(error);
 };
 
-export const handleResponse = (response: AxiosResponse, logger: Logger): AxiosResponse => {
+export const handleResponse = (response: AxiosResponse, logger: pino.Logger): AxiosResponse => {
   logger.info(response.data);
   return response;
 };
 
 export const handleResponseError = async (
   error: AxiosError<unknown>,
-  logger: Logger
+  logger: pino.Logger
 ): Promise<AxiosError<unknown>> => {
   logger.error(`[ Response Error ] ${error.code || 'UNKNOWN'} | ${error.message}`);
   return Promise.reject(error);
