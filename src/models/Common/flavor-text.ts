1e4398115ea8027c77408c71f4f66e521494dd68
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-12-02 02:37:36 -0300

    fix(models): flavor text remove version

--- src/models/Common/flavor-text.ts
+++ src/models/Common/flavor-text.ts
@@ -8,6 +8,4 @@ export interface FlavorText {
   flavor_text: string;
   /** The language this name is in */
   language: NamedAPIResource;
-  /** The game version this flavor text is extracted from */
-  version: NamedAPIResource;
 }
