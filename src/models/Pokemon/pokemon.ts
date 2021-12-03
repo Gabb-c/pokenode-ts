d8d4cd45b44e23935bc83048290cb402b85c8e0b
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-12-02 23:56:53 -0300

    fix(models): pokemo form sprites types

--- src/models/Pokemon/pokemon.ts
+++ src/models/Pokemon/pokemon.ts
@@ -633,19 +633,19 @@ export interface PokemonForm {
  */
 export interface PokemonFormSprites {
   /** The default depiction of this Pokémon form from the front in battle */
-  front_default: string;
+  front_default: string | null;
   /** The female depiction of this Pokémon form from the front in battle */
   front_female: string | null;
   /** The shiny depiction of this Pokémon form from the front in battle */
-  front_shiny: string;
+  front_shiny: string | null;
   /** The shiny female depiction of this Pokémon form from the front in battle */
   front_shiny_female: string | null;
   /** The default depiction of this Pokémon form from the back in battle */
-  back_default: string;
+  back_default: string | null;
   /** The female depiction of this Pokémon form from the back in battle */
   back_female: string | null;
   /** The shiny depiction of this Pokémon form from the back in battle */
-  back_shiny: string;
+  back_shiny: string | null;
   /** The shiny female depiction of this Pokémon form from the back in battle */
   back_shiny_female: string | null;
 }
