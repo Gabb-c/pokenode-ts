5af33dc1615a98cda6126d5a304f705e31ee2838
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-07-11 17:47:51 -0300

    feat(constants): update evolution triggers

--- src/constants/evolutions.ts
+++ src/constants/evolutions.ts
@@ -3,5 +3,10 @@ export enum EvolutionTriggers {
   TRADE = 2,
   USE_ITEM = 3,
   SHED = 4,
-  OTHER = 5,
+  SPIN = 5,
+  TOWER_OF_DARKNESS = 6,
+  TOWER_OF_WATER = 7,
+  THREE_CRITICAL_HITS = 8,
+  TAKE_DAMAGE = 9,
+  OTHER = 10,
 }
