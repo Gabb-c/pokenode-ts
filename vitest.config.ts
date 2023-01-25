99c23f19ea59b45745ae15c0003966d95cf8cd53
Gabb-c <gdgabrieldacunha71@gmail.com>
2023-01-24 21:15:28 -0300

    fix: remove unused files and modules

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,3 @@
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-  test: {
-    // ...
-  },
-})
+export default defineConfig({ test: { clearMocks: true, name: 'pokenode-ts' } });
