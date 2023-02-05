6962674d4b78cb8446dd437d628b63d064610e41
Gabb-c <gdgabrieldacunha71@gmail.com>
2023-02-04 22:32:11 -0300

    feat(deps): update axios

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,4 +3,24 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Pokenode-ts',
   description: 'Just playing around.',
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en'
+    },
+  },
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Clients',
+        collapsed: true,
+        items: [
+          {
+            text: 'Berry Client',
+            link: '/clients/berry-client'
+          }
+        ]
+      }
+    ]
+  }
 });
