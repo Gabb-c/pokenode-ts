86b188317ecc79f61e6567b2e6729e49aad3a498
Gabb-c <gdgabrieldacunha71@gmail.com>
2023-02-16 11:07:47 -0300

    build: update build and package config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,15 @@
 import isCI from 'is-ci';
 import { defineConfig } from 'tsup';
 import { myBanner } from './banner';
+import { peerDependencies } from './package.json';
+
+const EXTERNAL_DEPS = Object.keys(peerDependencies as object);
 
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'lib',
   splitting: true,
-  external: ['axios', 'pino', 'pino-pretty', 'axios-cache-interceptor'],
+  external: EXTERNAL_DEPS,
   sourcemap: !isCI,
   clean: !isCI,
   dts: true,
