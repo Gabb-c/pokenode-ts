965603fa43249ce720d4e243811e08f3b19786eb
Gabb-c <gdgabrieldacunha71@gmail.com>
2023-03-07 20:14:45 -0300

    chore: update dev deps and add size-limit

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 import { myBanner } from './banner';
 import { peerDependencies } from './package.json';
 
-const EXTERNAL_DEPS = Object.keys(peerDependencies as object);
+const EXTERNAL_DEPS = Object.keys(peerDependencies as Record<string, string>);
 
 export default defineConfig({
   entry: ['src/index.ts'],
