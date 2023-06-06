ef396139b5bd36858dcf71ad5162d422eb68c35b
Gabriel <gdgabrieldacunha71@gmail.com>
2023-06-06 17:41:36 +0000

    chore(test): update vitest deps and add path alias config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({ test: { testTimeout: 10_000 } });
+export default defineConfig({ test: { testTimeout: 10_000 }, plugins: [tsconfigPaths()] });
