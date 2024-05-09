410dfbff3775dc934071cab102fd0c4c41301e33
Gabb-c <gdgabrieldacunha71@gmail.com>
2024-05-09 08:23:03 -0300

    test: add server unhandledException event handler

--- tests/utils/setup.ts
+++ tests/utils/setup.ts
@@ -6,6 +6,13 @@ const HANDLERS = [...BERRY_HANDLERS];
 
 const server = setupServer(...HANDLERS);
 
+// Events
+server.events.on("unhandledException", ({ request: { method, url }, error }) => {
+  console.log(`${method} ${url} errored! See details below.`);
+  console.error(error);
+});
+
+// Vitest hooks
 beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
