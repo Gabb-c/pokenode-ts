9c189e38837f3f53d8b2c8707aa81132cf4ba8f7
Gabb-c <gdgabrieldacunha71@gmail.com>
2024-06-23 15:16:40 -0300

    chore: remove code duplications

--- tests/berry/mocks/data.ts
+++ tests/berry/mocks/data.ts
@@ -13,34 +13,6 @@ export const MOCK_BERRY = {
       },
       potency: 10,
     },
-    {
-      flavor: {
-        name: "dry",
-        url: "https://pokeapi.co/api/v2/berry-flavor/2/",
-      },
-      potency: 0,
-    },
-    {
-      flavor: {
-        name: "sweet",
-        url: "https://pokeapi.co/api/v2/berry-flavor/3/",
-      },
-      potency: 0,
-    },
-    {
-      flavor: {
-        name: "bitter",
-        url: "https://pokeapi.co/api/v2/berry-flavor/4/",
-      },
-      potency: 0,
-    },
-    {
-      flavor: {
-        name: "sour",
-        url: "https://pokeapi.co/api/v2/berry-flavor/5/",
-      },
-      potency: 0,
-    },
   ],
   growth_time: 3,
   id: 1,
@@ -69,82 +41,6 @@ export const MOCK_BERRY_LIST = {
       name: "cheri",
       url: "https://pokeapi.co/api/v2/berry/1/",
     },
-    {
-      name: "chesto",
-      url: "https://pokeapi.co/api/v2/berry/2/",
-    },
-    {
-      name: "pecha",
-      url: "https://pokeapi.co/api/v2/berry/3/",
-    },
-    {
-      name: "rawst",
-      url: "https://pokeapi.co/api/v2/berry/4/",
-    },
-    {
-      name: "aspear",
-      url: "https://pokeapi.co/api/v2/berry/5/",
-    },
-    {
-      name: "leppa",
-      url: "https://pokeapi.co/api/v2/berry/6/",
-    },
-    {
-      name: "oran",
-      url: "https://pokeapi.co/api/v2/berry/7/",
-    },
-    {
-      name: "persim",
-      url: "https://pokeapi.co/api/v2/berry/8/",
-    },
-    {
-      name: "lum",
-      url: "https://pokeapi.co/api/v2/berry/9/",
-    },
-    {
-      name: "sitrus",
-      url: "https://pokeapi.co/api/v2/berry/10/",
-    },
-    {
-      name: "figy",
-      url: "https://pokeapi.co/api/v2/berry/11/",
-    },
-    {
-      name: "wiki",
-      url: "https://pokeapi.co/api/v2/berry/12/",
-    },
-    {
-      name: "mago",
-      url: "https://pokeapi.co/api/v2/berry/13/",
-    },
-    {
-      name: "aguav",
-      url: "https://pokeapi.co/api/v2/berry/14/",
-    },
-    {
-      name: "iapapa",
-      url: "https://pokeapi.co/api/v2/berry/15/",
-    },
-    {
-      name: "razz",
-      url: "https://pokeapi.co/api/v2/berry/16/",
-    },
-    {
-      name: "bluk",
-      url: "https://pokeapi.co/api/v2/berry/17/",
-    },
-    {
-      name: "nanab",
-      url: "https://pokeapi.co/api/v2/berry/18/",
-    },
-    {
-      name: "wepear",
-      url: "https://pokeapi.co/api/v2/berry/19/",
-    },
-    {
-      name: "pinap",
-      url: "https://pokeapi.co/api/v2/berry/20/",
-    },
   ],
 } as NamedAPIResourceList;
 
@@ -153,34 +49,6 @@ export const MOCK_BERRY_FIRMNESS = {
     {
       name: "pecha",
       url: "https://pokeapi.co/api/v2/berry/3/",
-    },
-    {
-      name: "pamtre",
-      url: "https://pokeapi.co/api/v2/berry/32/",
-    },
-    {
-      name: "belue",
-      url: "https://pokeapi.co/api/v2/berry/35/",
-    },
-    {
-      name: "wacan",
-      url: "https://pokeapi.co/api/v2/berry/38/",
-    },
-    {
-      name: "tanga",
-      url: "https://pokeapi.co/api/v2/berry/46/",
-    },
-    {
-      name: "charti",
-      url: "https://pokeapi.co/api/v2/berry/47/",
-    },
-    {
-      name: "chilan",
-      url: "https://pokeapi.co/api/v2/berry/52/",
-    },
-    {
-      name: "rowap",
-      url: "https://pokeapi.co/api/v2/berry/64/",
     },
   ],
   id: 1,
@@ -192,41 +60,6 @@ export const MOCK_BERRY_FIRMNESS = {
         url: "https://pokeapi.co/api/v2/language/1/",
       },
       name: "とてもやわらかい",
-    },
-    {
-      language: {
-        name: "zh-Hant",
-        url: "https://pokeapi.co/api/v2/language/4/",
-      },
-      name: "很柔軟",
-    },
-    {
-      language: {
-        name: "fr",
-        url: "https://pokeapi.co/api/v2/language/5/",
-      },
-      name: "Très tendre",
-    },
-    {
-      language: {
-        name: "es",
-        url: "https://pokeapi.co/api/v2/language/7/",
-      },
-      name: "Muy blanda",
-    },
-    {
-      language: {
-        name: "en",
-        url: "https://pokeapi.co/api/v2/language/9/",
-      },
-      name: "Very Soft",
-    },
-    {
-      language: {
-        name: "zh-Hans",
-        url: "https://pokeapi.co/api/v2/language/12/",
-      },
-      name: "很柔软",
     },
   ],
 } as BerryFirmness;
@@ -268,202 +101,6 @@ export const MOCK_BERRY_FLAVOR = {
       },
       potency: 10,
     },
-    {
-      berry: {
-        name: "leppa",
-        url: "https://pokeapi.co/api/v2/berry/6/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "oran",
-        url: "https://pokeapi.co/api/v2/berry/7/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "persim",
-        url: "https://pokeapi.co/api/v2/berry/8/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "lum",
-        url: "https://pokeapi.co/api/v2/berry/9/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "razz",
-        url: "https://pokeapi.co/api/v2/berry/16/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "pinap",
-        url: "https://pokeapi.co/api/v2/berry/20/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "pomeg",
-        url: "https://pokeapi.co/api/v2/berry/21/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "qualot",
-        url: "https://pokeapi.co/api/v2/berry/23/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "hondew",
-        url: "https://pokeapi.co/api/v2/berry/24/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "nomel",
-        url: "https://pokeapi.co/api/v2/berry/30/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "belue",
-        url: "https://pokeapi.co/api/v2/berry/35/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "rindo",
-        url: "https://pokeapi.co/api/v2/berry/39/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "shuca",
-        url: "https://pokeapi.co/api/v2/berry/43/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "charti",
-        url: "https://pokeapi.co/api/v2/berry/47/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "apicot",
-        url: "https://pokeapi.co/api/v2/berry/57/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "cheri",
-        url: "https://pokeapi.co/api/v2/berry/1/",
-      },
-      potency: 10,
-    },
-    {
-      berry: {
-        name: "chople",
-        url: "https://pokeapi.co/api/v2/berry/41/",
-      },
-      potency: 15,
-    },
-    {
-      berry: {
-        name: "figy",
-        url: "https://pokeapi.co/api/v2/berry/11/",
-      },
-      potency: 15,
-    },
-    {
-      berry: {
-        name: "occa",
-        url: "https://pokeapi.co/api/v2/berry/36/",
-      },
-      potency: 15,
-    },
-    {
-      berry: {
-        name: "tamato",
-        url: "https://pokeapi.co/api/v2/berry/26/",
-      },
-      potency: 20,
-    },
-    {
-      berry: {
-        name: "tanga",
-        url: "https://pokeapi.co/api/v2/berry/46/",
-      },
-      potency: 20,
-    },
-    {
-      berry: {
-        name: "babiri",
-        url: "https://pokeapi.co/api/v2/berry/51/",
-      },
-      potency: 25,
-    },
-    {
-      berry: {
-        name: "starf",
-        url: "https://pokeapi.co/api/v2/berry/59/",
-      },
-      potency: 30,
-    },
-    {
-      berry: {
-        name: "liechi",
-        url: "https://pokeapi.co/api/v2/berry/53/",
-      },
-      potency: 30,
-    },
-    {
-      berry: {
-        name: "spelon",
-        url: "https://pokeapi.co/api/v2/berry/31/",
-      },
-      potency: 30,
-    },
-    {
-      berry: {
-        name: "petaya",
-        url: "https://pokeapi.co/api/v2/berry/56/",
-      },
-      potency: 30,
-    },
-    {
-      berry: {
-        name: "lansat",
-        url: "https://pokeapi.co/api/v2/berry/58/",
-      },
-      potency: 30,
-    },
-    {
-      berry: {
-        name: "enigma",
-        url: "https://pokeapi.co/api/v2/berry/60/",
-      },
-      potency: 40,
-    },
   ],
   contest_type: {
     name: "cool",
@@ -479,34 +116,6 @@ export const MOCK_BERRY_FLAVOR = {
       },
       name: "からい",
     },
-    {
-      language: {
-        name: "zh-Hant",
-        url: "https://pokeapi.co/api/v2/language/4/",
-      },
-      name: "辣",
-    },
-    {
-      language: {
-        name: "fr",
-        url: "https://pokeapi.co/api/v2/language/5/",
-      },
-      name: "Épicé",
-    },
-    {
-      language: {
-        name: "en",
-        url: "https://pokeapi.co/api/v2/language/9/",
-      },
-      name: "Spicy",
-    },
-    {
-      language: {
-        name: "zh-Hans",
-        url: "https://pokeapi.co/api/v2/language/12/",
-      },
-      name: "辣",
-    },
   ],
 } as BerryFlavor;
 
@@ -518,22 +127,6 @@ export const MOCK_BERRY_FLAVOR_LIST = {
     {
       name: "spicy",
       url: "https://pokeapi.co/api/v2/berry-flavor/1/",
-    },
-    {
-      name: "dry",
-      url: "https://pokeapi.co/api/v2/berry-flavor/2/",
-    },
-    {
-      name: "sweet",
-      url: "https://pokeapi.co/api/v2/berry-flavor/3/",
-    },
-    {
-      name: "bitter",
-      url: "https://pokeapi.co/api/v2/berry-flavor/4/",
-    },
-    {
-      name: "sour",
-      url: "https://pokeapi.co/api/v2/berry-flavor/5/",
     },
   ],
 } as NamedAPIResourceList;
